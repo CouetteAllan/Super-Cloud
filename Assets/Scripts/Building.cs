a1d1149951213e82403ca935c929ebcc215e4501
Allan <couetteallan@gmail.com>
2023-06-20 10:24:56 +0200

    more fire overtime

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 
 public class Building : MonoBehaviour, IRainable
 {
-    [SerializeField] private GameObject _fire;
+    [SerializeField] private GameObject[] _fire;
+    private int _currentTick;
+    private int _currentFireIndex;
 
     public enum BuildingState
     {
@@ -19,14 +21,17 @@ public class Building : MonoBehaviour, IRainable
     
     private void Start()
     {
-        TimeTickSystemDataHandler.OnTick += OnTick;
-        _fire.SetActive(true);
-        _currentState = BuildingState.OnFire;
+        ChangeBuildingState(BuildingState.OnFire);
     }
 
     private void OnTick(int tick)
     {
-        Debug.Log($"Tick: {tick}");
+        _currentTick++;
+        if(_currentTick % 25 == 0) //every 25 ticks, so every 5 seconds
+        {
+            IncreaseFire(++_currentFireIndex);
+            Debug.Log("Fire !" + _currentFireIndex);
+        }
     }
 
     public void TryToGetWet(PlayerController playerController)
@@ -34,11 +39,48 @@ public class Building : MonoBehaviour, IRainable
         if(_currentState == BuildingState.OnFire)
         {
             //Commence à éteindre le feu et diminuer la "vie" du feu au fur et à mesure qu'on pluite dessus
-            Debug.Log("JE NE SUIS PLUS EN FEU");
-            _currentState = BuildingState.Normal;
-            _fire.SetActive(false);
-            TimeTickSystemDataHandler.OnTick -= OnTick;
+            ChangeBuildingState(BuildingState.Normal);
 
+        }
+    }
+
+    private void ChangeBuildingState(BuildingState newState)
+    {
+        if (newState == _currentState)
+            return;
+        _currentState = newState;
+        switch (_currentState)
+        {
+            case BuildingState.Normal:
+                SetFireActive(false);
+                TimeTickSystemDataHandler.OnTick -= OnTick;
+                Debug.Log("JE NE SUIS PLUS EN FEU");
+                break;
+            case BuildingState.OnFire:
+                TimeTickSystemDataHandler.OnTick += OnTick;
+                IncreaseFire(0);
+                break;
+            case BuildingState.Destroyed:
+                break;
+        }
+    }
+
+    private void SetFireActive(bool active)
+    {
+        foreach (var fire in _fire)
+        {
+            fire.SetActive(active);
+        }
+    }
+
+    private void IncreaseFire(int fireMaxActive)
+    {
+        if (fireMaxActive > _fire.Length - 1)
+            return;
+
+        for(int i = 0; i <=  fireMaxActive; i++)
+        {
+            _fire[i].SetActive(true);
         }
     }
 }
